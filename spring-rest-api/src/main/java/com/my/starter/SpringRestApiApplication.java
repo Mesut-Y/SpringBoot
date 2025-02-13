4de4761d0f58093bb37b8d98472ff46e0e59e083
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-13 12:28:48 +0300

    starter main classına @ComponentScan annotation eklendi.

--- spring-rest-api/src/main/java/com/my/starter/SpringRestApiApplication.java
+++ spring-rest-api/src/main/java/com/my/starter/SpringRestApiApplication.java
@@ -2,7 +2,11 @@ package com.my.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Component;
 
+@ComponentScan(basePackages = {"com.my"})  //tüm proje içindeki annotasyonların bean oluşturmasını sağlar.
 @SpringBootApplication
 public class SpringRestApiApplication {
 
