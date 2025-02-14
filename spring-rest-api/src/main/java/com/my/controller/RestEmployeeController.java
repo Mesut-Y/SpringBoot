c04cb97ce9a44ba6bc3eca1f9dad099a7de4cdd6
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-14 22:38:21 +0300

    @RequestParam controller tamamlandı.

--- spring-rest-api/src/main/java/com/my/controller/RestEmployeeController.java
+++ spring-rest-api/src/main/java/com/my/controller/RestEmployeeController.java
@@ -6,27 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.my.model.Employee;
 import com.my.services.EmployeeService;
 
 @RestController
-@RequestMapping("/rest/api") // requestler için kök adres belirlendi.
+@RequestMapping("/rest/api/employee") // requestler için kök adres belirlendi.
 public class RestEmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService; // @Autowired bean ulaşmasını sağlar. Olmazsa nullpointer.
 
-	@GetMapping(path = "/employee-list") // get isteği urlsi belirlendi. Bu url için getAllEmployeeList çalışacak.
-	public List<Employee> getAllEmployeeList() {
+	//@RequestParam annotation ile kullanmak doğrudur. Bu hali sadece örnek
+	@GetMapping(path = "/list") // get isteği urlsi belirlendi. Bu url için getAllEmployeeList çalışacak.
+	public List<Employee> getAllEmployeeList() { 
 		return employeeService.getAllEmployeeList();
 	}
 
-	@GetMapping(path = "/employee-list/{id}") // GetMapping isteğine id parametresini eklendi.
-	public Employee getEmployeeById(@PathVariable(name = "id", required = true) String id) { // name pathteki değişken
-																								// ismi ve require ile
-																								// zorunlu
+	@GetMapping(path = "/list/{id}") // GetMapping isteğine id parametresini eklendi.
+	public Employee getEmployeeById(@PathVariable(name = "id", required = true) String id) { // name pathteki değişken ismi ve require ile zorunlu
 		return employeeService.getEmployeeById(id);
+	}
+	
+	@GetMapping(path = "with-params")
+	public List<Employee> getEmployeeWithParams(@RequestParam(name = "firstName") String firstName, //firstName urldeki ile aynı olmalı
+												@RequestParam(name = "lastName", required = false) String lastName){ //required-zorunluluk= false default true
+		System.out.println(firstName + " " + lastName);
+		return null;
 	}
 }
