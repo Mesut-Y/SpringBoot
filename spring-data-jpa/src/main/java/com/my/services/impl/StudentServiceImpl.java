aff870ae4b548716bea006468037f5d5ed2608bc
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-23 19:55:50 +0300

    PathVariable annotation ile id'e göre student nesnesi getirildi.

--- spring-data-jpa/src/main/java/com/my/services/impl/StudentServiceImpl.java
+++ spring-data-jpa/src/main/java/com/my/services/impl/StudentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.my.services.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,16 @@ public class StudentServiceImpl implements IStudentService{
 	public List<Student> getAllStudents() {
 		return studentRepository.findAll();
 		
+	}
+
+	@Override
+	public Student getStudentById(Integer id) {
+		Optional<Student> opt = studentRepository.findById(id);
+		if (opt.isPresent()) //  !opt.isEmpty()
+		{
+			return opt.get();
+		}
+		return null;
 	}
 
 }
