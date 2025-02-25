e8b825227ceb1475ac58c2dfbb45341f94b8f7c2
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-25 23:00:10 +0300

    Customer ve Adress arasında çift yönlü ilişki oluşturuldu.
    mappedBy kullanılarak ilişki sahibi belirlendi.

--- spring-data-jpa/src/main/java/com/my/entities/Adress.java
+++ spring-data-jpa/src/main/java/com/my/entities/Adress.java
@@ -1,5 +1,32 @@
 package com.my.entities;
 
-public class Adress {
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "adress")
+public class Adress {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@OneToOne(mappedBy = "adress")
+	private Customer customer;
+	
 }
