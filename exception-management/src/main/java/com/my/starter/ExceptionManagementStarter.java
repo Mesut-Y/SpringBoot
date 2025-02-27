c49de5d06c3e29c693e1107e79e60c188b4f4ed5
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-27 15:56:00 +0300

    Exceptiron Handling EntitiyScan hazırlandı
    Tabloları oluşturması için projeyi @Entitiy taraması yapıldı.

--- exception-management/src/main/java/com/my/starter/ExceptionManagementStarter.java
+++ exception-management/src/main/java/com/my/starter/ExceptionManagementStarter.java
@@ -2,7 +2,9 @@ package com.my.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(value = "com.my")
 @SpringBootApplication
 public class ExceptionManagementStarter {
 
