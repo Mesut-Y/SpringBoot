f4cece378f7effad47f745337361a73ba10257f1
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-27 12:57:41 +0300

    application.properties isim değişikliği uygulandı.

---
+++ spring-data-jpa/src/main/java/com/my/configuration/GlobalProperties.java
@@ -0,0 +1,11 @@
+package com.my.configuration;
+
+public class GlobalProperties {
+
+	
+	private String url;
+	
+	private String userName;
+	
+	private String password;
+}
--- spring-data-jpa/src/main/java/com/my/starter/SpringDataJpaApplication.java
+++ spring-data-jpa/src/main/java/com/my/starter/SpringDataJpaApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.my") //Tüm projedeki entity araştırması yapılır.
 @ComponentScan(basePackages = "com.my")
 @EnableJpaRepositories(basePackages = "com.my")   
+@PropertySource(value = "classpath:app.properties")
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
