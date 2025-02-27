14f9a72b47dda094dc81a6f393eae235abfe41e3
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-27 15:58:34 +0300

    Exceptiron Handling ManyToOne hazırlandı
    Employee tablosunda department_,id oluştu.

--- exception-management/src/main/java/com/my/entities/Employee.java
+++ exception-management/src/main/java/com/my/entities/Employee.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,4 +30,7 @@ public class Employee {
 	
 	@Column(name = "worked_year")
 	private String wordkedYear;
+	
+	@ManyToOne
+	private Department department;
 }
