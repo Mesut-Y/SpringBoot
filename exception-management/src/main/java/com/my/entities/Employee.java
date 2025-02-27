1f3d70c37a0aaba9bdbfe3f1d78059bc3f381fdc
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-27 15:49:22 +0300

    Exceptiron Handling entities employee sınıfı hazırlandı

--- exception-management/src/main/java/com/my/entities/Employee.java
+++ exception-management/src/main/java/com/my/entities/Employee.java
@@ -1,5 +1,32 @@
 package com.my.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "surname")
+	private String surname;
+	
+	@Column(name = "worked_year")
+	private String wordkedYear;
 }
