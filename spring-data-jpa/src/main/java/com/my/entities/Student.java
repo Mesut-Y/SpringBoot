fd576fd345aa33fe95d5f55e2c0d4e49386ee150
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-02-27 11:22:28 +0300

    Starting ManyToMany relations with table called Student-Course

--- spring-data-jpa/src/main/java/com/my/entities/Student.java
+++ spring-data-jpa/src/main/java/com/my/entities/Student.java
@@ -1,5 +1,6 @@
 package com.my.entities;
 
+import java.util.List;
 import java.util.Locale.IsoCountryCode;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,6 +13,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,5 +48,11 @@ public class Student {
 	
 	@Column(name = "tc_kimlik_no")
 	private String tckno;
+	
+	@ManyToMany
+	@JoinTable(name = "student_course",
+	joinColumns = @JoinColumn(name="student_id"),
+	inverseJoinColumns = @JoinColumn(name="course_id"))
+	private List<Course> courses;
 
 }
