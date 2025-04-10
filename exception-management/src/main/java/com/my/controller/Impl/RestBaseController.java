fa2900c8f6f5ed8f4c103ec11ab95f4e204d1a98
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-10 11:31:33 +0300

    Root Entity hazırlandı.

--- exception-management/src/main/java/com/my/controller/Impl/RestBaseController.java
+++ exception-management/src/main/java/com/my/controller/Impl/RestBaseController.java
@@ -1,5 +1,7 @@
 package com.my.controller.Impl;
 
+import com.my.entities.RootEntitiy;
+
 public class RestBaseController {
-	
+	//RootEntitiy<T> rootEntity = new RootEntitiy<>()
 }
--- exception-management/src/main/java/com/my/entities/RootEntitiy.java
+++ exception-management/src/main/java/com/my/entities/RootEntitiy.java
@@ -1,10 +1,22 @@
 package com.my.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class RootEntitiy<E> {
+@AllArgsConstructor
+@NoArgsConstructor
+public class RootEntitiy<T> {
 	private boolean status;
-	private E data;
-	private String message;
+	private T data;
+	private String errorMessage;
+	
+	public RootEntitiy<T> ok(){
+		return (new RootEntitiy<T>(true, data, null));
+	}
+	
+	public RootEntitiy<T> error(){
+		return (new RootEntitiy<T>(false, null, "Controller Hata Mesajı."));
+	}
 }
