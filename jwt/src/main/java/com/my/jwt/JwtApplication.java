58a3a464d56a8a666a7e884c7485d5fcfa281592
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-15 20:43:41 +0300

    JWT projesine başlandı.

---
+++ jwt/src/main/java/com/my/jwt/JwtApplication.java
@@ -0,0 +1,13 @@
+package com.my.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+}
