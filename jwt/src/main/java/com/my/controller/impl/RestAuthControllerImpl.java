dcf33d72fa82d1bc609a0214daa9eb593fce1316
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-16 23:16:29 +0300

    Register servisi için paketler oluşturuldu.

---
+++ jwt/src/main/java/com/my/controller/impl/RestAuthControllerImpl.java
@@ -0,0 +1,10 @@
+package com.my.controller.impl;
+
+import org.springframework.stereotype.Controller;
+
+import com.my.controller.IRestAuthController;
+
+@Controller
+public class RestAuthControllerImpl implements IRestAuthController{
+
+}
---
+++ jwt/src/main/java/com/my/service/impl/AuthServiceImpl.java
@@ -0,0 +1,11 @@
+package com.my.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.my.service.IAuthService;
+
+@Service
+public class AuthServiceImpl implements IAuthService {
+
+	
+}
