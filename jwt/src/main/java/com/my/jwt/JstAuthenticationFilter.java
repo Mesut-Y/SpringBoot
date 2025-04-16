7f3555b52c610e75bbfc69b4805d66038bd574d0
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-16 13:49:29 +0300

    JwtAuthenticationFilter hazırlandı.

---
+++ jwt/src/main/java/com/my/jwt/JstAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.my.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JstAuthenticationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String header;
+		String token;
+		String username;
+
+		header = request.getHeader("Authorization");
+
+		if (header == null) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		token = header.substring(7); // Bearer s34AerFEYadwet...
+		try {
+			username = jwtService.getUsernameByToken(token); // take token's username
+			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+				UserDetails userDetails = userDetailsService.loadUserByUsername(username); // check between db's
+																							// username and token's
+																							// username
+				if (userDetails != null && jwtService.isTokenExpired(token)) {
+					// user passed filter
+					UsernamePasswordAuthenticationToken authentication = 
+							new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+
+					authentication.setDetails(userDetails);
+					SecurityContextHolder.getContext().setAuthentication(authentication);
+				}
+			}
+		} catch (ExpiredJwtException e) {
+			System.out.println("Token süresi dolmuştur: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Genel bir hata oluştu." + e.getMessage());
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
--- jwt/src/main/java/com/my/jwt/JwtService.java
+++ jwt/src/main/java/com/my/jwt/JwtService.java
@@ -2,7 +2,6 @@ package com.my.jwt;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.concurrent.ConcurrentHashMap.KeySetView;
 import java.util.function.Function;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,13 +19,21 @@ public class JwtService {
 	private static final String SECRET_KEY = "VoUs4Gg25Q+XXKHOKYYQ4p3fdJi2BrDReiiuHq4HRhQ=";
 
 	public String generateToken(UserDetails userDetails) {
-		return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2))
-				.signWith(getKey(), SignatureAlgorithm.HS256).compact();
+		return Jwts.builder()
+				.setSubject(userDetails.getUsername()) //username would be token's subject
+				.setIssuedAt(new Date()) //creating date
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2)) //1000ms *60*60*2 = 2 hours
+				.signWith(getKey(), SignatureAlgorithm.HS256) //HS256 algorithm 
+				.compact(); 
 	}
 
 	public <T> T exportToken(String token, Function<Claims, T> claimsFunction) {
-		Claims claims = Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token).getBody();
+		Claims claims = Jwts
+				.parserBuilder()
+				.setSigningKey(getKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
 
 		return claimsFunction.apply(claims);
 	}
@@ -40,8 +47,8 @@ public class JwtService {
 		return new Date().before(expiredDate); // if current time is less than expiredDate True, else False
 	}
 
-	public Key getKey() {
-		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+	public Key getKey() { //generate key with SECRET_KEY
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY); 
 		return Keys.hmacShaKeyFor(keyBytes);
 	}
 }
